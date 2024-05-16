d627dd7424528d08b80dc71afd4c3a4b0778dc4c
mackardyback <mackardyback@internet.ru>
2024-05-16 20:10:52 +0300

    fixed errors

--- MyFirstGame.java
+++ MyFirstGame.java
@@ -4,20 +4,21 @@ public class MyFirstGame {
     public static void main(String[] args) throws InterruptedException {
         int computerNum = 12;
         int playerNum = 0;
-        while (playerNum != computerNum) {
-            Random r = new Random();
-            playerNum = r.nextInt(1, 100);
 
-            System.out.println(playerNum + "\r");
+        Random r = new Random();
+        while (playerNum != computerNum) {
+            playerNum = r.nextInt(1, 101);
+
+            System.out.println(playerNum);
             Thread.sleep(1000);
 
             if (playerNum > computerNum) {
                 System.out.println("Число " + playerNum + " больше того, что загадал компьютер");
             } else if (playerNum < computerNum) {
                 System.out.println("Число " + playerNum + " меньше того, что загадал компьютер");
-            } else {
-                System.out.println("Вы победили!");
             }
         }
+
+        System.out.println("Вы победили!");
     }
 }
