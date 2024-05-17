ad83756cb735d037c6614d1438eedb236bfa4c83
mackardyback <mackardyback@internet.ru>
2024-05-17 22:38:31 +0300

    added new directory Lesson_2

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+    String name = "Али";
+    String gender = "М";
+    int age = 20;
+    int height = 181;
+    float weight = 77.5;
+
+
+    void go() {
+        System.out.println("Идёт");
+    }
+
+    void sit() {
+        System.out.println("Сидит");
+    }
+
+    void speak() {
+        System.out.println("Говорит");
+    }
+
+    void run() {
+        System.out.println("Бежит");
+    }
+
+    void learnJava() {
+        System.out.println("Учит java");
+    }
+}
