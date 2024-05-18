89121ec11fcb9ddd90ea318dae1a241bcb6793ee
Muhammad Yanarsaev <mackardyback@internet.ru>
2024-05-18 11:10:26 +0300

    created Wolf class and added variables and methods

---
+++ Lesson_2/Wolf.java
@@ -0,0 +1,27 @@
+class Wolf {
+    String name;
+    String gender;
+    String color;
+    int age;
+    float weight;
+
+    void go() {
+        System.out.println("Идет");
+    }
+
+    void sit() {
+        System.out.println("Сидит");
+    }
+
+    void run() {
+        System.out.println("Бежит");
+    }
+
+    void howl() {
+        System.out.println("Воет");
+    }
+
+    void hunting() {
+        System.out.println("Охотится");
+    }
+}
