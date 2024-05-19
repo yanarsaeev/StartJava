118c78741af8fe7c93a81dc0ba1877959797dc5f
Muhammad Yanarsaev <mackardyback@internet.ru>
2024-05-19 15:05:13 +0300

    added getters/setters

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,27 +1,67 @@
 class Wolf {
-    String name;
-    String gender;
-    String color;
-    int age;
-    float weight;
+    private String name;
+    private String gender;
+    private String color;
+    private int age;
+    private float weight;
 
-    void go() {
+    public void go() {
         System.out.println("Идет");
     }
 
-    void sit() {
+    public void sit() {
         System.out.println("Сидит");
     }
 
-    void run() {
+    public void run() {
         System.out.println("Бежит");
     }
 
-    void howl() {
+    public void howl() {
         System.out.println("Воет");
     }
 
-    void hunting() {
+    public void hunting() {
         System.out.println("Охотится");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
     }
 }
