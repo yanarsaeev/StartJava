59c5fd79203af214bc43e7bcd7fc07c877d128da
Muhammad Yanarsaev <mackardyback@internet.ru>
2024-05-24 22:34:57 +0300

    added tests for Calculator class

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class CalculatorTest {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         do {
-            Scanner scanner = new Scanner(System.in);
             System.out.println("Введите первое число:");
             int firstNum = scanner.nextInt();
             scanner.nextLine();
@@ -11,11 +11,26 @@ public class CalculatorTest {
             char sign = scanner.nextLine().charAt(0);
             System.out.println("Введите второе число:");
             int secondNum = scanner.nextInt();
-            scanner.close();
-
+            scanner.nextLine();
             Calculator expression1 = new Calculator(firstNum, sign, secondNum);
             expression1.calculate();
             expression1.printAnswer();
-        } while ();
+
+            boolean isRepeat = true;
+            while (true) {
+                System.out.println("Хотите продолжить вычисления? [yes/no]");
+                String repeat = scanner.nextLine();
+                if (repeat.equals("yes")) {
+                    break;
+                } else if (repeat.equals("no")) {
+                    isRepeat = false;
+                    break;
+                }
+            }
+
+            if (isRepeat == false) {
+                break;
+            }
+        } while (true);
     }
 }
