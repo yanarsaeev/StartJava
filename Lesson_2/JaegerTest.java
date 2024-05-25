7e7ead4bdeef82377503356c0b289f587c179c20
Muhammad Yanarsaev <mackardyback@internet.ru>
2024-05-26 00:20:33 +0300

    writed tests for Jaeger class

---
+++ Lesson_2/JaegerTest.java
@@ -0,0 +1,24 @@
+public class JaegerTest {
+    public static void main(String[] args) {
+        Jaeger jaeger1 = new Jaeger("Striker Eureka", "Mark-5", "Australia");
+        System.out.println(jaeger1.getMark());
+        System.out.println(jaeger1.getModelName());
+        System.out.println(jaeger1.getOrigin());
+
+        jaeger1.setPilots(2);
+        jaeger1.setRockets(3);
+        jaeger1.useRockets(2);
+
+        Jaeger jaeger2 = new Jaeger();
+        jaeger2.setModelName("Gipsy Avenger");
+        jaeger2.setMark("Mark-6");
+        jaeger2.setBodyLanguage("Street Fighter");
+        System.out.println(jaeger2.getModelName() + 
+                "\n" + jaeger2.getMark() + 
+                "\n" + jaeger2.getBodyLanguage());
+        jaeger2.pilotsCount(3);
+        System.out.println(jaeger2.getPilots());
+        jaeger2.setRockets(10);
+        jaeger2.useRockets(5);
+    }
+}
