f8d64a01bdde3b206dfa3d3f1535b47b1db81393
mackardyback <mackardyback@internet.ru>
2024-05-29 22:07:35 +0300

    updated test

--- Lesson_2/GuessNumberTest.java
+++ Lesson_2/GuessNumberTest.java
@@ -3,21 +3,43 @@ import java.util.Scanner;
 public class GuessNumberTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Player player1 = new Player("Isa");
-        Player player2 = new Player("Ali");
+        Player player1 = new Player(scanner.next());
+        Player player2 = new Player(scanner.next());
 
-        GuessNumber guessNumber = new GuessNumber(player1, player2);
-        guessNumber.startGame();
+        GuessNumber game = new GuessNumber(player1, player2);
+        // game.start();
 
-        boolean isRepeat = true;
-        while (isRepeat) {
-            System.out.println("Хотите продолжить игру? [yes/no]");
-            String newGame = scanner.nextLine();
+        // boolean isRepeat = true;
+        // while (isRepeat) {
+        //     System.out.println("Хотите продолжить игру? [yes/no]");
+        //     String newGame = scanner.next();
 
-            if (newGame.equals("yes")) {
-                guessNumber.startGame();
-            } else if (newGame.equals("no")) {
-                isRepeat = false;
+        //     if (newGame.equals("yes")) {
+        //         guessNumber.startGame();
+        //     } else if (newGame.equals("no")) {
+        //         isRepeat = false;
+        //         break;
+        //     }
+        // }
+
+        String playerAnswer = "yes";
+        while (playerAnswer.equals("yes")) {
+            game.start();
+
+            boolean isRepeat = true;
+            while (isRepeat) {
+                System.out.println("Хотите продолжить игру? [yes/no]");
+                playerAnswer = scanner.next();
+
+                if (playerAnswer.equals("no")) {
+                    isRepeat = false;
+                    break;
+                } else if (playerAnswer.equals("yes")) {
+                    break;
+                }
+            }
+
+            if (!isRepeat) {
                 break;
             }
         }
