582eaebf4e6d9d1e76e4a9a4ee0cb19b1b6fd05d
Muhammad Yanarsaev <mackardyback@internet.ru>
2024-06-01 22:03:13 +0300

    fixed test

--- Lesson_2/GuessNumberTest.java
+++ Lesson_2/GuessNumberTest.java
@@ -3,44 +3,21 @@ import java.util.Scanner;
 public class GuessNumberTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("player1 name: ");
         Player player1 = new Player(scanner.next());
+        System.out.print("player2 name: ");
         Player player2 = new Player(scanner.next());
 
         GuessNumber game = new GuessNumber(player1, player2);
-        // game.start();
-
-        // boolean isRepeat = true;
-        // while (isRepeat) {
-        //     System.out.println("Хотите продолжить игру? [yes/no]");
-        //     String newGame = scanner.next();
-
-        //     if (newGame.equals("yes")) {
-        //         guessNumber.startGame();
-        //     } else if (newGame.equals("no")) {
-        //         isRepeat = false;
-        //         break;
-        //     }
-        // }
 
         String playerAnswer = "yes";
         while (playerAnswer.equals("yes")) {
             game.start();
-
-            boolean isRepeat = true;
-            while (isRepeat) {
+            System.out.println("Хотите продолжить игру? [yes/no]");
+            playerAnswer = scanner.next();
+            while (!playerAnswer.equals("yes") && !playerAnswer.equals("no")) {
                 System.out.println("Хотите продолжить игру? [yes/no]");
                 playerAnswer = scanner.next();
-
-                if (playerAnswer.equals("no")) {
-                    isRepeat = false;
-                    break;
-                } else if (playerAnswer.equals("yes")) {
-                    break;
-                }
-            }
-
-            if (!isRepeat) {
-                break;
             }
         }
     }
