9efa7379e4cbd224088ec4f9b8f33de1f95ab998
mackardyback <mackardyback@internet.ru>
2024-06-02 14:05:32 +0300

    fixed message output and exponentiation

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -3,8 +3,6 @@ class Calculator {
     private int arg1;
     private int arg2;
 
-    double result = 1.0;
-
     public Calculator(int arg1, char sign, int arg2) {
         this.arg1 = arg1;
         this.sign = sign;
@@ -12,52 +10,50 @@ class Calculator {
     }
 
     void calculate() {
+        double result = 1.0;
         switch (sign) {
             case '+':
                 result = arg1 + arg2;
-                printResult();
+                System.out.println(arg1 + " " + sign + " " + arg2 + " = " + result);
                 break;
             case '-':
                 result = arg1 - arg2;
-                printResult();
+                System.out.println(arg1 + " " + sign + " " + arg2 + " = " + result);
                 break;
             case '/':
                 if (arg2 == 0) {
                     System.out.println("Ошибка: деление на ноль запрещено");
                 } else {
                     result = arg1 / arg2;
+                    System.out.println(arg1 + " " + sign + " " + arg2 + " = " + result);
                 }
-                printResult();
                 break;
             case '*':
                 result = arg1 * arg2;
-                printResult();
+                System.out.println(arg1 + " " + sign + " " + arg2 + " = " + result);
                 break;
             case '%':
-                result = arg1 % arg2;
-                printResult();
+                if (arg2 == 0) {
+                    System.out.println("Ошибка: деление на ноль запрещено");
+                } else {
+                    result = arg1 % arg2;
+                    System.out.println(arg1 + " " + sign + " " + arg2 + " = " + result);
+                }
                 break;
             case '^':
-                if (arg2 > 0) {
-                    for (int i = 0; i < arg2; i++) {
-                        result *= arg1;
-                    }
-                } else if (arg2 < 0) {
-                    arg2 = -arg2;
-                    for (int i = 0; i < arg2; i++) {
-                        result *= arg1;
-                    }
+                for (int i = 0; i < Math.abs(arg2); i++) {
+                    result *= arg1;
+                }
+
+                if (arg2 < 0) {
                     result = 1.0 / result;
                 }
-                printResult();
+                System.out.println(arg1 + " " + sign + " " + arg2 + " = " + result);
                 break;
             default:
-                System.out.println("Ошибка: операция \"" + sign + "\" не поддерживается");
+                System.out.println("Ошибка: операция \"" + sign + "\" не поддерживается" +
+                        "\nДоступны следующие операции: +, -, *, /, ^, %");
                 break;
         }
-    }
-
-    void printResult() {
-        System.out.println("Ответ: " + result);
     }
 }
