dc8c275156af8b5c8d223a18ccaad1055a22929e
Muhammad Yanarsaev <mackardyback@internet.ru>
2024-06-03 21:15:02 +0300

    updated calculate() method and added raiseToPower() method

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -11,49 +11,55 @@ class Calculator {
 
     void calculate() {
         double result = 1.0;
+        String res = arg1 + " " + sign + " " + arg2 + " = ";
         switch (sign) {
             case '+':
                 result = arg1 + arg2;
-                System.out.println(arg1 + " " + sign + " " + arg2 + " = " + result);
                 break;
             case '-':
                 result = arg1 - arg2;
-                System.out.println(arg1 + " " + sign + " " + arg2 + " = " + result);
                 break;
             case '/':
                 if (arg2 == 0) {
                     System.out.println("Ошибка: деление на ноль запрещено");
                 } else {
                     result = arg1 / arg2;
-                    System.out.println(arg1 + " " + sign + " " + arg2 + " = " + result);
                 }
                 break;
             case '*':
                 result = arg1 * arg2;
-                System.out.println(arg1 + " " + sign + " " + arg2 + " = " + result);
                 break;
             case '%':
                 if (arg2 == 0) {
                     System.out.println("Ошибка: деление на ноль запрещено");
                 } else {
                     result = arg1 % arg2;
-                    System.out.println(arg1 + " " + sign + " " + arg2 + " = " + result);
                 }
                 break;
             case '^':
-                for (int i = 0; i < Math.abs(arg2); i++) {
-                    result *= arg1;
-                }
-
-                if (arg2 < 0) {
-                    result = 1.0 / result;
-                }
-                System.out.println(arg1 + " " + sign + " " + arg2 + " = " + result);
+                raiseToPower();
                 break;
             default:
                 System.out.println("Ошибка: операция \"" + sign + "\" не поддерживается" +
                         "\nДоступны следующие операции: +, -, *, /, ^, %");
                 break;
         }
+
+        if (sign != '^') {
+            res += raiseToPower();
+        } else {
+            res += result;
+        }
+
+        System.out.println(res);
+    }
+
+    double raiseToPower() {
+        double result = 1.0;
+        for (int i = 0; i < Math.abs(arg2); i++) {
+            result *= arg1;
+        }
+        result = (arg2 < 0) ? (1.0 / result) : result;
+        return result;
     }
 }
