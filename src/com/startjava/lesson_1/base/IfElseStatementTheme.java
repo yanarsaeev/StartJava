ac7a22197c779840f07ae5d72ec6acaf5b5a57ea
mackardyback <mackardyback@internet.ru>
2024-06-10 16:09:50 +0300

    specified package

--- src/com/startjava/lesson_1/base/IfElseStatementTheme.java
+++ src/com/startjava/lesson_1/base/IfElseStatementTheme.java
@@ -1,3 +1,5 @@
+package com.startjava.lesson_1.base;
+
 import java.math.*;
 
 public class IfElseStatementTheme {
--- src/com/startjava/lesson_1/base/VariablesTheme.java
+++ src/com/startjava/lesson_1/base/VariablesTheme.java
@@ -1,3 +1,5 @@
+package com.startjava.lesson_1.base;
+
 public class VariablesTheme {
     public static void main(String[] args) {
         System.out.println("1. Вывод характеристик компьютера");
