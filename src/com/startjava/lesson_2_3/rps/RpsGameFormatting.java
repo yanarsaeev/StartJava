2b7bbf390eb760de6bf08983c9dff124f8fee458
Muhammad Yanarsaev <mackardyback@internet.ru>
2024-06-10 21:57:55 +0300

    added RpsGameFormatting.java

--- src/com/startjava/lesson_2_3/rps/RpsGameFormatting.java
+++ src/com/startjava/lesson_2_3/rps/RpsGameFormatting.java
@@ -1,2 +1,78 @@
 package com.startjava.lesson_2_3.rps;
 
+import java.util.Random;
+import java.util.Scanner;
+
+public class RpsGameFormatting {
+    public static void main(String[] args) throws InterruptedException {
+        Random r = new Random();
+        Scanner console = new Scanner(System.in);
+        String name1 = inputName(console);
+        String name2 = inputName(console);
+
+        int position = generatePosition(name1, r);
+        String sign1 = defineSign(position);
+        showSigns(sign1);
+
+        position = generatePosition(name2, r);
+        String sign2 = defineSign(position);
+        showSigns(sign2);
+
+        defineWinner(name1, sign1, name2, sign2);
+    }
+
+    private static final String ROCK = "R";
+    private static final String SCISSORS = "S";
+    private static final String PAPER = "P";
+
+    private static String inputName(Scanner console) {
+        System.out.println("Введите имя игрока: ");
+        return console.nextLine();
+    }
+
+    private static int generatePosition(String name1, Random r) {
+        System.out.println("Ход " + name1 + ": ");
+        return r.nextInt(1, 100);
+    }
+
+    private static String defineSign(int position) {
+        String sign = ROCK;
+        if (position > 66) {
+            sign = ROCK;
+        } else if (position > 33) {
+            sign = ROCK;
+        }
+
+        return sign;
+    }
+
+    private static void showSigns(String sign) throws InterruptedException {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(ROCK + "\r");
+            Thread.sleep(100);
+            System.out.println(SCISSORS + "\r");
+            Thread.sleep(100);
+            System.out.println(PAPER + "\r");
+            Thread.sleep(100);
+        }
+        System.out.println(sign);
+    }
+
+    private static void defineWinner(String name1, String sign1, String name2, String sign2) {
+        if (sign1.equals(sign2)) {
+            System.out.println("\nПобедила дружба!");
+            return;
+        }
+
+        boolean isEqualName1 = sign1.equals(ROCK) && sign2.equals(SCISSORS) ||
+                sign1.equals(SCISSORS) && sign2.equals(PAPER) ||
+                sign1.equals(PAPER) && sign2.equals(ROCK);
+
+        if (isEqualName1) {
+            System.out.println("\nПобедил - " + name1);
+        } else {
+            System.out.println("\nПобедил - " + name2);
+        }
+    }
+}
+
