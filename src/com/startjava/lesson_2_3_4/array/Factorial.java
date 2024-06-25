fca96097ff6d3bce7b58c7afe633380639496d84
Muhammad Yanarsaev <mackardyback@internet.ru>
2024-06-25 22:32:27 +0300

    fixed errors

--- src/com/startjava/lesson_2_3_4/array/Factorial.java
+++ src/com/startjava/lesson_2_3_4/array/Factorial.java
@@ -8,24 +8,25 @@ public class Factorial {
         findFactorial(new int[]{-22, -0});
     }
 
-    private static void findFactorial(int... numbersToProcess) {
-        if (numbersToProcess == null) {
+    private static void findFactorial(int... numbersToFactorial) {
+        if (numbersToFactorial == null) {
             System.out.println("[!] Null");
         } else {
             long result = 1;
-            for (int i = 0; i < numbersToProcess.length; i++) {
-                if (numbersToProcess[i] > 0) {
-                    System.out.print(numbersToProcess[i] + "! = ");
-                    for (int j = 1; j <= numbersToProcess[i]; j++) {
+            int numbersToFactorialLength = numbersToFactorial.length;
+            for (int i = 0; i < numbersToFactorialLength; i++) {
+                if (numbersToFactorial[i] > 0) {
+                    System.out.print(numbersToFactorial[i] + "! = ");
+                    for (int j = 1; j <= numbersToFactorial[i]; j++) {
                         System.out.print(j == 1 ? j : " * " + j);
                         result *= j;
                     }
                     System.out.println(" = " + result);
                     result = 1;
-                } else if (numbersToProcess[i] == 0) {
-                    System.out.println(numbersToProcess[i] + "! = 0");
+                } else if (numbersToFactorial[i] == 0) {
+                    System.out.println(numbersToFactorial[i] + "! = 0");
                 } else {
-                    System.out.println("Ошибка: факториал " + numbersToProcess[i] + "! не определен");
+                    System.out.println("Ошибка: факториал " + numbersToFactorial[i] + "! не определен");
                 }
             }
         }
--- src/com/startjava/lesson_2_3_4/array/Reversion.java
+++ src/com/startjava/lesson_2_3_4/array/Reversion.java
@@ -10,20 +10,21 @@ public class Reversion {
         reverse(new int[]{13, 8, 5, 3, 2, 1, 1});
     }
 
-    private static void reverse(int[] numbersToProcess) {
-        if (numbersToProcess == null) {
-            System.out.println("Вы ввели некорректное значение (" + numbersToProcess + ")");
+    private static void reverse(int[] numbersToReverse) {
+        if (numbersToReverse == null) {
+            System.out.println("Вы ввели некорректное значение (" + numbersToReverse + ")");
         } else {
-            System.out.printf("%3sДо реверса: %s", " ", Arrays.toString(numbersToProcess));
-            int numberToProcessReducedLength = numbersToProcess.length - 1;
-            for (int i = 0; i < numbersToProcess.length / 2; i++) {
-                int swap = numbersToProcess[i];
-                numbersToProcess[i] = numbersToProcess[numberToProcessReducedLength];
-                numbersToProcess[numberToProcessReducedLength] = swap;
-                numberToProcessReducedLength--;
+            System.out.printf("%3sДо реверса: %s", " ", Arrays.toString(numbersToReverse));
+            int numberToReverseLength = numbersToReverse.length;
+            int numberToReverseReduceLength = numberToReverseLength - 1;
+            for (int i = 0; i < numberToReverseLength / 2; i++) {
+                int swap = numbersToReverse[i];
+                numbersToReverse[i] = numbersToReverse[numberToReverseReduceLength];
+                numbersToReverse[numberToReverseReduceLength] = swap;
+                numberToReverseReduceLength--;
             }
             System.out.println();
-            System.out.println("После реверса: " + Arrays.toString(numbersToProcess));
+            System.out.println("После реверса: " + Arrays.toString(numbersToReverse));
         }
     }
 }
