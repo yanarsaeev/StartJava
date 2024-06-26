3948ce1cd096ad6407cafd6673d7dbc6f3a7c5c9
mackardyback <mackardyback@internet.ru>
2024-06-26 18:49:07 +0300

    fixed errors and renamed classes

---
+++ src/com/startjava/lesson_2_3_4/array/FactorialСalculation.java
@@ -0,0 +1,34 @@
+package com.startjava.lesson_2_3_4.array;
+
+public class FactorialСalculation {
+    public static void main(String[] args) {
+        calcFactorial(new int[0]);
+        calcFactorial(null);
+        calcFactorial(new int[]{8, 0, 9});
+        calcFactorial(new int[]{-3, 1, 7, 13});
+        calcFactorial(new int[]{-22, -0});
+    }
+
+    private static void calcFactorial(int... toFactorial) {
+        if (toFactorial == null || toFactorial.length == 0) {
+            System.out.println("Вы ввели некорректное значение (null or length == 0)");
+            return;
+        }
+        long result = 1;
+        for (int i = 0; i < toFactorial.length; i++) {
+            if (toFactorial[i] > 0) {
+                System.out.print(toFactorial[i] + "! = ");
+                for (int j = 1; j <= toFactorial[i]; j++) {
+                    System.out.print(j == 1 ? j : " * " + j);
+                    result *= j;
+                }
+                System.out.println(" = " + result);
+                result = 1;
+            } else if (toFactorial[i] == 0) {
+                System.out.println(toFactorial[i] + "! = 0");
+            } else {
+                System.out.println("Ошибка: факториал " + toFactorial[i] + "! не определен");
+            }
+        }
+    }
+}
---
+++ src/com/startjava/lesson_2_3_4/array/Reverser.java
@@ -0,0 +1,28 @@
+package com.startjava.lesson_2_3_4.array;
+
+import java.util.Arrays;
+
+public class Reverser {
+    public static void main(String[] args) {
+        reverse(new int[0]);
+        reverse(null);
+        reverse(new int[]{6, 8, 9, 1});
+        reverse(new int[]{13, 8, 5, 3, 2, 1, 1});
+    }
+
+    private static void reverse(int[] toReverse) {
+        if (toReverse == null) {
+            System.out.println("Вы ввели некорректное значение (" + toReverse + ")");
+            return;
+        }
+        System.out.printf("%3sДо реверса: %s", " ", Arrays.toString(toReverse));
+        int length = toReverse.length;
+        for (int i = 0; i < length; i++) {
+            length--;
+            int swap = toReverse[i];
+            toReverse[i] = toReverse[length];
+            toReverse[length] = swap;
+        }
+        System.out.println("\nПосле реверса: " + Arrays.toString(toReverse));
+    }
+}
