4af96e058b91e8c145e88fab4f2b3c148f6f9672
mackardyback <mackardyback@internet.ru>
2024-07-04 23:58:07 +0300

    fixed error

--- src/com/startjava/lesson_2_3_4/array/AsciiPyramidPrinter.java
+++ src/com/startjava/lesson_2_3_4/array/AsciiPyramidPrinter.java
@@ -2,17 +2,24 @@ package com.startjava.lesson_2_3_4.array;
 
 public class AsciiPyramidPrinter {
     public static void main(String[] args) {
-        printPyramid('0', '9', true);
-        printPyramid('/', '!', false);
-        printPyramid('A', 'J', false);
+        printPyramid('9', '0', true);
+        printPyramid('/', '!', true);
+        printPyramid('A', 'J', true);
     }
 
     private static void printPyramid(char startChar, char endChar, boolean ascending) {
         int charRange = Math.abs(startChar - endChar) + 1;
         StringBuilder pyramid = new StringBuilder();
 
+        char ch;
+        if ((int) startChar > (int) endChar) {
+            ch = startChar;
+            startChar = endChar;
+            endChar = ch;
+        }
+
         for (int i = 0; i < charRange; i++) {
-            int currentChar = ascending ? endChar - i : startChar + i;
+            int currentChar = ascending ? startChar + i : endChar - i;
 
             for (int j = 0; j < charRange - i - 1; j++) {
                 pyramid.append(" ");
