83e06248891217314fb6e4882d48dcc57beea871
mackardyback <mackardyback@internet.ru>
2024-07-09 20:10:55 +0300

    updated print gallow and fixed errors

--- src/com/startjava/lesson_2_3_4/array/GallowsGame.java
+++ src/com/startjava/lesson_2_3_4/array/GallowsGame.java
@@ -19,10 +19,10 @@ public class GallowsGame {
         char[] mask = new char[hiddenWord.length()];
         Arrays.fill(mask, '_');
 
-        char[] correctUsedLetters = new char[16];
+        char[] correctUsedLetters = new char[5];
         int correctUsedLettersCount = 0;
 
-        char[] uncorrectUsedLetters = new char[17];
+        char[] uncorrectUsedLetters = new char[28];
         int uncorrectUsedLettersCount = 0;
 
         int lives = 5;
