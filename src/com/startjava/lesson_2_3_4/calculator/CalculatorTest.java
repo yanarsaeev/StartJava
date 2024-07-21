82af93a0a49034ee56fbb32082a8646f0c2678c9
mackardyback <mackardyback@internet.ru>
2024-07-21 10:50:28 +0300

    updated cycle work

--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -6,16 +6,18 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String playerAnswer = "yes";
-        while ("yes".equals(playerAnswer)) {
-            System.out.print("Введите математическое выражение: ");
-            String expression = scanner.nextLine();
+        while (true) {
+            if ("yes".equals(playerAnswer)) {
+                System.out.print("Введите математическое выражение: ");
+                String expression = scanner.nextLine();
 
-            Calculator.calculate(expression);
+                Calculator.calculate(expression);
 
-            System.out.print("Хотите продолжить вычисления? [yes / no] ");
-            playerAnswer = scanner.nextLine().toLowerCase();
-
-            if (!"yes".equals(playerAnswer) && !"no".equals(playerAnswer)) {
+                System.out.print("Хотите продолжить вычисления? [yes / no] ");
+                playerAnswer = scanner.nextLine().toLowerCase();
+            } else if ("no".equals(playerAnswer)) {
+                break;
+            } else {
                 System.out.print("Введите корректный ответ [yes / no]: ");
                 playerAnswer = scanner.nextLine().toLowerCase();
             }
