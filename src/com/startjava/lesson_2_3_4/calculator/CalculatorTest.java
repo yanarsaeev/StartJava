29de83312cae60c0c5c5e3ce44ec361a79a6568a
mackardyback <mackardyback@internet.ru>
2024-07-21 18:28:06 +0300

    added print expression

--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -1,5 +1,6 @@
 package com.startjava.lesson_2_3_4.calculator;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class CalculatorTest {
@@ -9,10 +10,26 @@ public class CalculatorTest {
         while (true) {
             if ("yes".equals(playerAnswer)) {
                 System.out.print("Введите математическое выражение: ");
-                String expression = scanner.nextLine();
 
-                Calculator.calculate(expression);
+                String[] expression = scanner
+                        .nextLine().trim()
+                        .replaceAll("\\s{2,}", " ").split(" ");
 
+                double result = Calculator.calculate(expression);
+
+                DecimalFormat decimalFormat;
+                if (result == 0.0) {
+                    decimalFormat = new DecimalFormat("0.#");
+                } else {
+                    decimalFormat = new DecimalFormat("0.###");
+                }
+
+                if (Double.isNaN(result)) {
+                    continue;
+                }
+
+                System.out.println(expression[0] + " " + expression[1] +
+                        " " + expression[expression.length - 1] + " = " + decimalFormat.format(result));
                 System.out.print("Хотите продолжить вычисления? [yes / no] ");
                 playerAnswer = scanner.nextLine().toLowerCase();
             } else if ("no".equals(playerAnswer)) {
