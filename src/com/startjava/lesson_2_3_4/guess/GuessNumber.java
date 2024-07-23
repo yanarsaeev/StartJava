53d12a2c4f40f277ae25d1a564769b95218ccfa7
mackardyback <mackardyback@internet.ru>
2024-07-23 22:24:38 +0300

    updated print, added new variables, added conditions

--- src/com/startjava/lesson_2_3_4/guess/GuessNumber.java
+++ src/com/startjava/lesson_2_3_4/guess/GuessNumber.java
@@ -1,28 +1,42 @@
 package com.startjava.lesson_2_3_4.guess;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class GuessNumber {
-    private Player player1;
-    private Player player2;
+    Player player1;
+    Player player2;
 
-    public GuessNumber(Player player1, Player player2) {
-        this.player1 = player1;
-        this.player2 = player2;
+    public GuessNumber(String name1, String name2) {
+        player1 = new Player(name1);
+        player2 = new Player(name2);
     }
 
     void start() {
+        int firstPlayerAttempts = 0;
+        int secondPlayerAttempts = 0;
+        int attempts = 10;
+        System.out.println("Игра началась! У каждого игрока по " + attempts + " попыток.");
         Random r = new Random();
         int targetNum = r.nextInt(1, 101);
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Игра началась!");
         while (true) {
             System.out.println("Вводит " + player1.getName() + " (игрок1): ");
-            player1.setNum(scanner.nextInt());
+            int firstPlayerNum = scanner.nextInt();
+            player1.setNum(firstPlayerNum);
+            firstPlayerAttempts++;
+            if (firstPlayerAttempts == attempts) {
+                System.out.println("У " + player1.getName() + " закончились попытки!");
+                Arrays.fill(player1.getNums(), 0, firstPlayerAttempts, 0);
+                break;
+            }
             String comparisonResult = "";
             if (player1.getNum() == targetNum) {
-                System.out.println(player1.getName() + " (игрок1) выиграл!");
+                System.out.println(player1.getName() + " угадал число " + firstPlayerNum + " с " +
+                        firstPlayerAttempts + "-й попытки!");
+                System.out.println(Arrays.toString(Arrays.copyOf(player1.getNums(), firstPlayerAttempts)));
+                Arrays.fill(player1.getNums(), 0, firstPlayerAttempts, 0);
                 break;
             }
 
@@ -36,9 +50,19 @@ public class GuessNumber {
                     " " + comparisonResult + " того, что загадал компьютер");
 
             System.out.println("Вводит " + player2.getName() + " (игрок2): ");
-            player2.setNum(scanner.nextInt());
+            int secondPlayerNum = scanner.nextInt();
+            player2.setNum(secondPlayerNum);
+            secondPlayerAttempts++;
+            if (secondPlayerAttempts == attempts) {
+                System.out.println("У " + player2.getName() + " закончились попытки!");
+                Arrays.fill(player2.getNums(), 0, secondPlayerAttempts, 0);
+                break;
+            }
             if (player2.getNum() == targetNum) {
-                System.out.println(player2.getName() + " (игрок2) выиграл!");
+                System.out.println(player2.getName() + " угадал число " + secondPlayerNum + " с " +
+                        secondPlayerAttempts + "-й попытки!");
+                System.out.println(Arrays.toString(Arrays.copyOf(player2.getNums(), secondPlayerAttempts)));
+                Arrays.fill(player2.getNums(), 0, secondPlayerAttempts, 0);
                 break;
             }
 
