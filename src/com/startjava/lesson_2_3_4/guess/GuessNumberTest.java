16fce93e4070ef2a82f4c757b918b0ef0476bfe9
mackardyback <mackardyback@internet.ru>
2024-08-01 12:35:26 +0300

    removed code duplication. the names of the players are now stored in an array

--- src/com/startjava/lesson_2_3_4/guess/GuessNumberTest.java
+++ src/com/startjava/lesson_2_3_4/guess/GuessNumberTest.java
@@ -5,14 +5,15 @@ import java.util.Scanner;
 public class GuessNumberTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("player1 name: ");
-        String name1 = scanner.nextLine();
-        System.out.print("player2 name: ");
-        String name2 = scanner.nextLine();
-        System.out.print("player3 name: ");
-        String name3 = scanner.nextLine();
+        String[] players = new String[3];
 
-        GuessNumber game = new GuessNumber(name1, name2, name3);
+        int count = 1;
+        for (int i = 0; i < players.length; i++) {
+            System.out.print("player" + count++ + " name: ");
+            players[i] = scanner.nextLine();
+        }
+
+        GuessNumber game = new GuessNumber(players);
 
         String playerAnswer = "yes";
         while (!"no".equals(playerAnswer)) {
