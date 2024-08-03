37228dac8fe09f2f58edae2f760a7c11a461e37e
mackardyback <mackardyback@internet.ru>
2024-08-03 23:20:54 +0300

    added enterNames(), constants

--- src/com/startjava/lesson_2_3_4/guess/GuessNumberTest.java
+++ src/com/startjava/lesson_2_3_4/guess/GuessNumberTest.java
@@ -3,17 +3,11 @@ package com.startjava.lesson_2_3_4.guess;
 import java.util.Scanner;
 
 public class GuessNumberTest {
+    private static final int PLAYERS_COUNT = 3;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] players = new String[3];
-
-        int count = 1;
-        for (int i = 0; i < players.length; i++) {
-            System.out.print("player" + count++ + " name: ");
-            players[i] = scanner.nextLine();
-        }
-
-        GuessNumber game = new GuessNumber(players);
+        GuessNumber game = new GuessNumber(enterNames());
 
         String playerAnswer = "yes";
         while (!"no".equals(playerAnswer)) {
@@ -25,5 +19,18 @@ public class GuessNumberTest {
             }
             playerAnswer = scanner.nextLine().toLowerCase();
         }
+    }
+
+    static String[] enterNames() {
+        Scanner scanner = new Scanner(System.in);
+        String[] players = new String[PLAYERS_COUNT];
+
+        int count = 1;
+        for (int i = 0; i < PLAYERS_COUNT; i++) {
+            System.out.print("player" + count++ + " name: ");
+            players[i] = scanner.nextLine();
+        }
+
+        return players;
     }
 }
